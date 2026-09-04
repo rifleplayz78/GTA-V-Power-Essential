2369d3e6ce0658b39f1440d53de5831904ac0490
RiflePlayz <ranaalihussainafzalkhan@gmail.com>
2026-09-04 22:40:03 +0500

    Implement logging for mod initialization and main thread
    Added logging functionality to track mod initialization and main thread activity.

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,19 @@
 #include <windows.h>
 #include <filesystem>
+#include <fstream>
 #include <string>
 #include "pugixml.hpp"
 
-// Forward system DirectInput8 exports so Windows accepts this proxy DLL
 #pragma comment(linker, "/export:DirectInput8Create=C:\\Windows\\System32\\dinput8.DirectInput8Create,@1")
 #pragma comment(linker, "/export:GetdfDIJoystick=C:\\Windows\\System32\\dinput8.GetdfDIJoystick,@2")
 
 namespace fs = std::filesystem;
 
-bool nativeUIOpen = false;
-bool menyooOpen = false;
-ULONGLONG lastF8Tick = 0;
-
-void InitializeModFolders(const fs::path& gameDir) {
-    fs::path scriptsDir = gameDir / "scripts";
-    fs::path menyooDir = gameDir / "menyooStuff";
-
-    if (!fs::exists(scriptsDir)) {
-        fs::create_directories(scriptsDir);
-    }
-    if (!fs::exists(menyooDir)) {
-        fs::create_directories(menyooDir);
-    }
-
-    fs::path vehicleListPath = menyooDir / "VehicleList.xml";
-    if (fs::exists(vehicleListPath)) {
-        pugi::xml_document doc;
-        if (doc.load_file(vehicleListPath.wstring().c_str())) {
-            // Placeholder for loading vehicle nodes into memory
-        }
-    }
-}
-
-void CheckInputKeybinds() {
-    if (GetAsyncKeyState(VK_F4) & 1) {
-        nativeUIOpen = !nativeUIOpen;
-    }
-
-    if (GetAsyncKeyState(VK_F8) & 1) {
-        ULONGLONG currentTick = GetTickCount64();
-        if (currentTick - lastF8Tick < 400) { 
-            menyooOpen = !menyooOpen;
-            lastF8Tick = 0; 
-        } else {
-            lastF8Tick = currentTick;
-        }
+void WriteLog(const fs::path& gameDir, const std::string& message) {
+    fs::path logPath = gameDir / "GTAV essen log.txt";
+    std::ofstream logFile(logPath, std::ios::app);
+    if (logFile.is_open()) {
+        logFile << message << "\n";
     }
 }
 
@@ -54,18 +22,10 @@ DWORD WINAPI MainThread(LPVOID lpParam) {
     GetModuleFileNameA(GetModuleHandleA("dinput8.dll"), dllPath, MAX_PATH);
     fs::path gameDir = fs::path(dllPath).parent_path();
 
-    InitializeModFolders(gameDir);
+    WriteLog(gameDir, "MainThread started successfully.");
 
     while (true) {
-        CheckInputKeybinds();
-        
-        if (nativeUIOpen) {
-            // Native UI draw loop logic
-        }
-        if (menyooOpen) {
-            // Menyoo UI draw loop logic
-        }
-
+        // Mod loop logic
         Sleep(5);
     }
     return 0;
@@ -73,10 +33,17 @@ DWORD WINAPI MainThread(LPVOID lpParam) {
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
     switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
+    case DLL_PROCESS_ATTACH: {
         DisableThreadLibraryCalls(hModule);
+        
+        char dllPath[MAX_PATH];
+        GetModuleFileNameA(hModule, dllPath, MAX_PATH);
+        fs::path gameDir = fs::path(dllPath).parent_path();
+        
+        WriteLog(gameDir, "DLL_PROCESS_ATTACH triggered. GTAV essen log.txt created.");
         CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, nullptr, 0, nullptr);
         break;
+    }
     case DLL_PROCESS_DETACH:
         break;
     }
